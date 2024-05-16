c082dbee89b0a699e493dea344b20642cfaa9e28
CaspLian <149088248+CaspLian@users.noreply.github.com>
2024-05-16 15:26:59 +0000

    444

--- conditionals.js
+++ conditionals.js
@@ -1,4 +1,4 @@
-let button = document.querySelector(".button");
+   let button = document.querySelector(".button");
 let californiaMessage = document.querySelector(".california");
 let illinoisMessage = document.querySelector(".illinois");
 let newyorkMessage = document.querySelector(".new-york");
@@ -9,7 +9,15 @@ button.addEventListener("click", function() {
 
     // CODE ALONG
     // 1. Write an if statement that would display the california message if a user says they're from "California".
-    
+    if (state === "California") {
+        californiaMessage.style.display = "block";
+    } else if (state === "Illinois") {
+        illinoisMessage.style.display = "block";
+    } else if (state === "New York") {
+        newyorkMessage.style.display = "block";
+    } else if (state === "Other") {
+        other.style.display = "block";
+    }
     // CODE SOLO
     // 2. Write an else if statement that would display the illinois message if a user says they're from "Illinois".
     
